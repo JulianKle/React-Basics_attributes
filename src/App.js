83b0f072714182148ed8928e8bfe1378e105ff40
Julian Klett <julian22a@googlemail.com>
2023-11-13 11:25:30 +0100

    components react

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+export function Article() {
+  return (
+    <article>
+      <h2 className="article__title">first steps with React</h2>
+      <label htmlFor="messages">messages</label>
+      <input id="messages"></input>
+      <a className="article__link" href="https://de.wikipedia.org/wiki/React">
+        More information about react
+      </a>
+    </article>
+  );
 }
